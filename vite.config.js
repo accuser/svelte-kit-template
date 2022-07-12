f103917f9d3ae312b79ff7c3e01e0c385a0bd446
Matthew Gibbons <matthew@southwestkoala.co.uk>
2022-07-12 06:49:49 +0100

    Bump

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [sveltekit()],
+	test: {
+		exclude: ['build', 'node_modules']
+	}
+};
+
+export default config;
